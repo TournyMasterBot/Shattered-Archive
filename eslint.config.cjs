9860c5e1f139fe9cfab4e1f5a4257dfeda4bc6e2
TournyMasterBot <20309653+TournyMasterBot@users.noreply.github.com>
2025-02-16 08:48:31 -0600

    Linting

---
+++ eslint.config.cjs
@@ -0,0 +1,19 @@
+module.exports = [
+  // This configuration applies only to TypeScript files
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: require("@typescript-eslint/parser"),
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+      prettier: require("eslint-plugin-prettier"),
+    },
+    rules: {
+      "prettier/prettier": "error",
+      // You can add additional TypeScript-specific rules here
+    },
+  },
+];
