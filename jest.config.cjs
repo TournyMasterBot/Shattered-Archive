3c81f562ca3a0a49d2cd6b490ed5c5ccd3cf5557
TournyMasterBot <20309653+TournyMasterBot@users.noreply.github.com>
2025-12-13 18:16:34 -0600

    Update jest.config.cjs

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,19 +3,28 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
 
-  // When each workspace runs with --rootDir .,
-  // this will point at its own src folder.
   roots: ['<rootDir>/src'],
 
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1", // map ./foo.js -> ./foo (so ts-jest can pick up foo.ts)
+    "^(\\.{1,2}/.*)\\.js$": "$1",
   },
+
   testMatch: [
     '**/__tests__/**/*.(test|spec).[tj]s?(x)',
     '**/?(*.)+(test|spec).[tj]s?(x)',
   ],
+
   transform: {
     '^.+\\.(t|j)sx?$': 'ts-jest',
   },
+
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: [
+    'json-summary',
+    'json',
+    'lcov',
+    'text',
+  ],
 };
