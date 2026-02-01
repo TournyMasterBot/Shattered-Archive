b0f9a3e2623ce05cc43ef06cba65da12aac8e285
TournyMasterBot <TournyMasterBot@users.noreply.github.com>
2026-01-31 20:51:07 -0600

    Update user-script-runtime-options.ts

--- apps/game-client/src/types/userscript-types/user-script-runtime-options.ts
+++ apps/game-client/src/types/userscript-types/user-script-runtime-options.ts
@@ -6,4 +6,19 @@ export interface UserScriptRuntimeOptions {
   sendCommand?: SendCommandFn;
   onScriptError?: (err: ScriptErrorInfo) => void;
   aliasSplitChar?: string;
+
+  /**
+   * Global variables assigned to languages
+   */
+  getGlobalVar?: (key: string) => unknown;
+  setGlobalVar?: (key: string, value: unknown) => void;
+  deleteGlobalVar?: (key: string) => void;
+
+  /**
+   * Named variables used for template expansion: "{NAME}"
+   * (resolved at runtime; can change at any time)
+   */
+  getNamedVar?: (name: string) => string | undefined;
+  setNamedVar?: (name: string, value: string) => void;
+  deleteNamedVar?: (name: string) => void;
 }
